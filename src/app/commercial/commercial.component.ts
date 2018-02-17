ab97e4b0c9f88101ee9d44705b6e4fb446c7223a
hsandstromOM <hosea@obviouslee.com>
2018-02-17 13:24:42 -0500

    project placeholders set up in comm and res pages

--- src/app/commercial/commercial.component.ts
+++ src/app/commercial/commercial.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-commercial',
@@ -6,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commercial.component.scss']
 })
 export class CommercialComponent implements OnInit {
+  closeResult: string;
+  currentJustify = 'center';
 
-  constructor() { }
+  constructor(private modalService: NgbModal, config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 6000;
+    config.wrap = true;
+    config.keyboard = true;
+   }
 
+  open(content) {
+   this.modalService.open(content).result.then((result) => {
+     this.closeResult = `Closed with: ${result}`;
+   }, (reason) => {
+     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+   });
+ }
+
+ private getDismissReason(reason: any): string {
+   if (reason === ModalDismissReasons.ESC) {
+     return 'by pressing ESC';
+   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+     return 'by clicking on a backdrop';
+   } else {
+     return  `with: ${reason}`;
+   }
+ }
   ngOnInit() {
   }
 
---
+++ src/custom.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//   $('.item-info').hide()
+//
+//   $('.item').mouseenter(function() {
+//     $('.item-info').fadeIn(300);
+//     $('.item-info').css("cursor", "pointer");
+//   });
+//
+//   $('.item').mouseleave(function() {
+//     $('.item-info').fadeOut(300)
+//   });
+//
+// });
