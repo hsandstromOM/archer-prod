8b0b51c3a08478ab71d380c6243cc4c04d8e52c5
hsandstromOM <hosea@obviouslee.com>
2018-02-21 20:34:09 -0500

    added in content

--- src/custom.js
+++ src/custom.js
@@ -1,13 +1,37 @@
-// $(document).ready(function() {
-//   $('.item-info').hide()
-//
-//   $('.item').mouseenter(function() {
-//     $('.item-info').fadeIn(300);
-//     $('.item-info').css("cursor", "pointer");
-//   });
-//
-//   $('.item').mouseleave(function() {
-//     $('.item-info').fadeOut(300)
-//   });
-//
-// });
+$(document).ready(function() {
+  $('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
