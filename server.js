ada4aeb0ccec64b6c2091142e05ef81c7b3c0b5f
hsandstromOM <hosea@obviouslee.com>
2018-02-25 20:48:15 -0500

    test express

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require('prerender-node'));
 
 
 app.use(express.static(__dirname + '/dist'));
